ac06a8b29d990b5cc04281765cdf9b96f4822c1c
D2Allaire <elishawitte@gmail.com>
2017-11-07 13:22:59 +0100

    Fix code header being added to all code blocks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,16 @@
 const pres = document.getElementsByTagName('pre');
 console.log(pres);
 pres.forEach((pre) => {
-  const icon = document.createElement('i');
-  icon.classList.add('mdi');
-  icon.classList.add('mdi-code-tags');
-  const span = document.createElement('span');
-  span.classList.add('code-header-icon');
-  span.appendChild(icon);
-  const header = document.createElement('div');
-  header.classList.add('code-header');
-  header.appendChild(span);
-  pre.parentNode.insertBefore(header, pre);
+  if(pre.firstChild.className.split(' ').some(c => /language-.*/.test(c))) {
+    const icon = document.createElement('i');
+    icon.classList.add('mdi');
+    icon.classList.add('mdi-code-tags');
+    const span = document.createElement('span');
+    span.classList.add('code-header-icon');
+    span.appendChild(icon);
+    const header = document.createElement('div');
+    header.classList.add('code-header');
+    header.appendChild(span);
+    pre.parentNode.insertBefore(header, pre);
+  }
 });
